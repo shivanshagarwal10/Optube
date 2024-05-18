b5bebf9c668a4955b72049c85d03b7ad97529f20
Shivansh Agarwal <shivanshagrawal21@gmail.com>
2024-05-18 23:27:29 +0530

    this.message was missing

--- utils/ApiError.js
+++ utils/ApiError.js
@@ -8,6 +8,7 @@ class ApiError extends Error{
         super(message)
         this.statusCode = statusCode
         this.data = this.data
+        this.message = message 
         this.success = false;
         this.errors = errors
 
